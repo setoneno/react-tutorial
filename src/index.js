9e9d9e82273d5be58a32fe05e413b8998cc6014f
setoneno <setone0222@gmail.com>
2020-02-26 16:17:09 +0900

    soft coding

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class Board extends React.Component {
     return (
       <Square
         value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
+        onClick={() => this.props.onClick(i)} key={i}
       />
     );
   }
@@ -23,21 +23,21 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {
+          Array(3).fill(0).map((row, i) => {
+            return (
+              <div className='board-row' key={i}>
+                {
+                  Array(3).fill(0).map((col, j) => {
+                    return (
+                      this.renderSquare(i * 3 + j)
+                    )
+                  })
+                }
+              </div>
+            )
+          })
+        }
       </div>
     );
   }
